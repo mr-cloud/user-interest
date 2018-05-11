32962139814e7aa75c0fcfa8355754de54d46a44
mr-cloud <zhangxin1992pm@gmail.com>
2018-05-11 08:40:00 +0800

    Modified examples building procedure.

--- preprocessing.py
+++ preprocessing.py
@@ -10,12 +10,12 @@ CLEAN_DATA_PATH = 'datahouse/clean-data'
 RAW_DATA_PATH = 'datahouse/raw-data'
 
 
-def build_photo_examples(face_filename, text_filename, examples_filename):
+def build_photo_examples(face_filename, text_filename, examples_name):
     # XXX
     pass
 
 
-def build_pop_examples(interact_filename, examples_filename):
+def build_pop_examples(interact_filename, examples_name):
     pass
 
 
@@ -24,9 +24,14 @@ def main():
         os.makedirs(CLEAN_DATA_PATH)
     build_photo_examples(os.path.join(RAW_DATA_PATH, 'train_face.txt'),
                          os.path.join(RAW_DATA_PATH, 'train_text.txt'),
-                         os.path.join(CLEAN_DATA_PATH, 'train_photo_examples.txt'))
+                         os.path.join(CLEAN_DATA_PATH, 'train_photo_examples'))
+    build_photo_examples(os.path.join(RAW_DATA_PATH, 'test_face.txt'),
+                         os.path.join(RAW_DATA_PATH, 'test_text.txt'),
+                         os.path.join(CLEAN_DATA_PATH, 'test_photo_examples'))
     build_pop_examples(os.path.join(RAW_DATA_PATH, 'train_interaction.txt'),
-                       os.path.join(CLEAN_DATA_PATH, 'train_pop_examples.txt'))
+                       os.path.join(CLEAN_DATA_PATH, 'train_pop_examples'))
+    build_pop_examples(os.path.join(RAW_DATA_PATH, 'test_interaction.txt'),
+                       os.path.join(CLEAN_DATA_PATH, 'test_pop_examples'))
     print('Examples building finished.')
 
 
