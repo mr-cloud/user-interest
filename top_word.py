959d6af9ef02552bfb524f4bd1f14507c07009c1
mr-cloud <zhangxin1992pm@gmail.com>
2018-05-27 00:04:27 +0800

    Modified log.

--- top_word.py
+++ top_word.py
@@ -29,10 +29,17 @@ vectorizer = CountVectorizer(analyzer=lambda x: x.split(','))
 counts = vectorizer.fit_transform(corpus)
 transformer = TfidfTransformer()
 tfidf = transformer.fit_transform(counts)
-print(tfidf[:5, :5])
+print('Trained TF-IDF, size: ', tfidf.shape)
 maxargs = np.argmax(tfidf, axis=1)
-with open(os.path.join(preprocessing_photos.CLEAN_DATA_PATH, 'photo_term.txt'), 'w') as output:
+print('maxargs size: ', maxargs.shape)
+n_photos = maxargs.shape[0]
+print('#photos={}'.format(n_photos))
+features = vectorizer.get_feature_names()
+print('#features={}'.format(len(features)))
+with open(os.path.join(preprocessing_photos.CLEAN_DATA_PATH, 'photo_topic.txt'), 'w') as output:
     for ind in range(maxargs.shape[0]):
-        output.write('{} {}'.format(photos_id[ind], vectorizer.get_feature_names()[maxargs[ind, 0]]))
-        output.write('\n')
+        if ind % 10000 == 0:
+            print('Writing #{}'.format(ind))
+        output.write('{} {}\n'.format(photos_id[ind], features[maxargs[ind, 0]]))
+        output.flush()
 print('Finished')
